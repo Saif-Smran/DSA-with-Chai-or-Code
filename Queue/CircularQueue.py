478733d626abf09bf58ac67584313f940924e697
Saif Smran <23303079@iubat.edu>
2026-01-11 14:40:13 +0600

    exam day break 1

--- Queue/CircularQueue.py
+++ Queue/CircularQueue.py
@@ -41,5 +41,6 @@ cq.enqueue(20)
 cq.enqueue(30)
 cq.enqueue(40)
 cq.enqueue(50)
-cq.enqueue(60)  # This will raise an exception: Queue is full
 print(cq.dequeue())  # Output: 10
+cq.enqueue(60)  # This will raise an exception: Queue is full
+print(cq.dequeue())  # Output: 20
